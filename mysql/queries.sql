f7e8cec2a699932f3693af8d234bfd5dd480ec1a
vincentclee <vincentlee.dolbydigital@yahoo.com>
2014-07-08 21:50:44 -0500

    Number 6 done

--- mysql/queries.sql
+++ mysql/queries.sql
@@ -43,4 +43,12 @@ SELECT * FROM Student,
 WHERE Student.id = alias.studId;
 
 -- 6. List the names of students who have taken all courses offered by department v8 (deptId).
-
+SELECT name FROM Student,
+	(SELECT studId
+	FROM Transcript
+		WHERE crsCode IN
+		(SELECT crsCode FROM Course WHERE deptId = @v8 AND crsCode IN (SELECT crsCode FROM Teaching))
+		GROUP BY studId
+		HAVING COUNT(*) = 
+			(SELECT COUNT(*) FROM Course WHERE deptId = @v8 AND crsCode IN (SELECT crsCode FROM Teaching))) as alias
+WHERE id = alias.studId;
