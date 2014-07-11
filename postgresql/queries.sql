4d8feaa595f8a7f72959583627aa4f27832b985a
TehMatty <tehmatty86@gmail.com>
2014-07-10 23:39:07 -0400

    Fixed all instances of'StdId' to 'StudId'

--- postgresql/queries.sql
+++ postgresql/queries.sql
@@ -39,7 +39,7 @@ where Student.id BETWEEN @v2 AND @v3;
 -- 3. List the names of students who have taken course v4 (crsCode).
 Select Student.name
 from Student, Transcript
-Where Student.id=Transcript.StdId AND Transcript.crsCode=@v4;
+Where Student.id=Transcript.StudId AND Transcript.crsCode=@v4;
 
 
 
@@ -47,7 +47,7 @@ Where Student.id=Transcript.StdId AND Transcript.crsCode=@v4;
 Select Student.name
 from Student, Transcript, Teaching, Professor
 where Transcript.crsCode= Teaching.crsCode 
-AND Student.id= Transcript.stdId
+AND Student.id= Transcript.studId
 AND Teaching.profID= Professor.ID
 AND Professor.name= @v5;
 
@@ -55,13 +55,13 @@ AND Professor.name= @v5;
 Select Student.name
 from Student, Transcript, Course
 where Transcript.crsCode= Course.crsCode
-and Student.id = Transcript.stdId 
+and Student.id = Transcript.studId 
 and Course.deptId = @v6 and Course.deptId <> @v7 ;
 
 -- 6. List the names of students who have taken all courses offered by department v8 (deptId).
 Select Student.name
 from Student, Transcript, Course
-where Transcript.stdId = Student.id 
+where Transcript.studId = Student.id 
 AND ALL Transcript.crsCode=Course.crsCode 
 AND Course.deptId= @v8;
 
